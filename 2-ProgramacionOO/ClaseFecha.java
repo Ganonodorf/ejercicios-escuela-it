ff960f6e011b5d7d81d091d3fe92d10c58028ad1
Ganonodorf <juanjogarridobascaran@gmail.com>
2025-01-20 11:49:15 +0100

    Ejercicio clase fecha hecho

---
+++ 2-ProgramacionOO/ClaseFecha.java
@@ -0,0 +1,12 @@
+class Fecha{
+	public Fecha hoy(){};
+	public char diaSemana(){};
+	public boolean festivo(){};
+	public int diasAnoRestantes(){};
+	public int diferenciaDias(Fecha fecha){};
+	public boolean mayor(Fecha fecha){};
+	public boolean igual(Fecha fecha){};
+	public void sumarDia(int dias){}; public void sumarMes(int mes){}; public void sumarAno(int ano){};
+	public boolean bisiesto(){};
+	public Estacion estacion(){};
+}
