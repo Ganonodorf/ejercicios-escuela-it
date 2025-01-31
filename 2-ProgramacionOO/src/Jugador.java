2f33fdcb5acfb703ddd356aa9d9fb2bf4cae1836
Juanjo Garrido Bascarán <juanjogarridobascaran@MacBook-Pro-de-Juanjo.local>
2025-01-31 14:48:57 +0100

    feat: Creación de media interfaz de Jugador

--- 2-ProgramacionOO/src/Jugador.java
+++ 2-ProgramacionOO/src/Jugador.java
@@ -1,9 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Jugador {
-
+	
+	public Jugador() {
+		
+	}
+	
 	public void Accionar(Baraja baraja, Montones montones) {
+		assert baraja != null;
+		assert montones != null;
+		
+		MostrarInfo();
+		
+		String input = RecogerInput();
+		
+		HacerAccion(input);
+	}
+
+	private void HacerAccion(String input) {
 		// TODO Auto-generated method stub
 		
+	}
+
+	private String RecogerInput() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		return br.toString();
+	}
+
+	private void MostrarInfo() {
+		System.out.println("Nueva carta: c \n");
+		System.out.println("Mover carta: número montón + posición carta desde abajo + nuevo número montón");
 	}
 
 	public boolean PuedeAccionar() {
@@ -15,5 +42,8 @@ public class Jugador {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
+	public static void main(String[] args) {
+		
+	}
 }
