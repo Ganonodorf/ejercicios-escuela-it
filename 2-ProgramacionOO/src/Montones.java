adae30325f1ee654e8f03993eb96869045127138
Juanjo Garrido Bascaran <juanjogarridobascaran@gmail.com>
2025-02-03 11:40:09 +0100

    feat: Añadir métodos Montones

--- 2-ProgramacionOO/src/Montones.java
+++ 2-ProgramacionOO/src/Montones.java
@@ -1,48 +1,103 @@
+import java.util.ArrayList;
 
 public class Montones {
 
-	public Montones(Baraja baraja) {
-		// TODO Auto-generated constructor stub
+	private ArrayList<ArrayList<Carta>> montonesCartas;
+	
+	private final int NUMERO_COLUMNAS = 7;
+	
+	public Montones() {
+		montonesCartas = new ArrayList<ArrayList<Carta>>();
+		
+		for(int i = 0; i < NUMERO_COLUMNAS; i++) {
+			montonesCartas.add(new ArrayList<Carta>());
+		}
 	}
 
 	public boolean EstanVacios() {
-		// TODO Auto-generated method stub
-		return false;
+		for(int i = 0; i < NUMERO_COLUMNAS; i++) {
+			if(!montonesCartas.get(i).isEmpty()){
+				return false;
+			}
+		}
+		return true;
 	}
 
 	public void Mostrar() {
-		// TODO Auto-generated method stub
+		int cartasEnColumnaMasLarga = ObtenerCartasEnColumnaMasLarga();
 		
+		String cartasEnColumnas = "";
+		
+		for(int i = 0; i < cartasEnColumnaMasLarga; i++) {
+			for(int j = 0; j < NUMERO_COLUMNAS; j++) {
+				if(montonesCartas.get(j).size() > i){
+					cartasEnColumnas += montonesCartas.get(j).get(i).Mostrar();
+				}
+				else {
+					cartasEnColumnas += "   ";
+				}
+				cartasEnColumnas += "  ";
+			}
+			cartasEnColumnas += "/n";
+		}
 	}
 
-	public void AnadirCarta(Object obtenerDescarte, char charAt) {
-		// TODO Auto-generated method stub
+	private int ObtenerCartasEnColumnaMasLarga() {
+		int cartasEnColumnaMasLarga = 0;
 		
+		for(int i = 0; i < NUMERO_COLUMNAS; i++) {
+			if(montonesCartas.get(i).size() > cartasEnColumnaMasLarga) {
+				cartasEnColumnaMasLarga = montonesCartas.get(i).size();
+			}
+		}
+		
+		return cartasEnColumnaMasLarga;
 	}
 
-	public boolean SePuedeAnadirCarta(Object obtenerDescarte, char charAt) {
-		// TODO Auto-generated method stub
+	public boolean SePuedeAnadirCarta(Carta cartaDescarte, int numeroMonton) {
+		Carta primeraCartaMonton = montonesCartas.get(numeroMonton - 1).getFirst();
+		
+		if(cartaDescarte.SePuedeColocarDebajo(primeraCartaMonton)) {
+			return true;
+		}
+		
 		return false;
 	}
 
-	public void MoverCartas(String string, String string2, String string3) {
+	public void AnadirCarta(Carta cartaDescarte, int numeroMonton) {
+		Carta primeraCartaMonton = montonesCartas.get(numeroMonton - 1).getFirst();
+		
+		montonesCartas.get(numeroMonton - 1).add(cartaDescarte);
+	}
+
+	public boolean SePuedeMoverCarta(int montonOrigen, int posicionCarta, int montonDestino) {
+		Carta cartaSeleccionada = montonesCartas.get(montonOrigen - 1).get(posicionCarta - 1);
+		Carta cartaDestino = montonesCartas.get(montonDestino - 1).getFirst();
+		if(cartaSeleccionada != null && cartaDestino != null && cartaSeleccionada.SePuedeColocarDebajo(cartaDestino)){
+			return true;
+		}
+		return false;
+	}
+
+	public void MoverCartas(int montonOrigen, int posicionCarta, int montonDestino) {
+		for(int i = posicionCarta - 1; i >= 0; i--) {
+			montonesCartas.get(montonDestino - 1).addFirst(montonesCartas.get(montonOrigen - 1).get(i));
+			montonesCartas.get(montonOrigen - 1).remove(i);
+		}
+	}
+
+	public boolean SePuedeMoverCartaAlPilar(Pilares pilares, int numeroMonton) {
+		return false;
+	}
+
+	public void CartaAlPilar(Pilares pilares, int numeroMonton) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	public void CartaAlPilar(Pilares pilares, char charAt) {
+	public void Repartir(Baraja baraja) {
 		// TODO Auto-generated method stub
 		
-	}
-
-	public boolean SePuedeMoverCartaAlPilar(Pilares pilares, char charAt) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	public boolean SePuedeMoverCarta(String string, String string2, String string3) {
-		// TODO Auto-generated method stub
-		return false;
 	}
 
 }
