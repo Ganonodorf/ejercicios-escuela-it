885e857283eefbe1da9a3ee0761cf9fc15c211b8
Juanjo Garrido Bascaran <juanjogarridobascaran@gmail.com>
2025-02-03 08:31:07 +0100

    refactor: Quitar funcionalidad duplicada en Jugador

--- 2-ProgramacionOO/src/Jugador.java
+++ 2-ProgramacionOO/src/Jugador.java
@@ -7,47 +7,7 @@ public class Jugador {
 	}
 
 	public String RecogerInput() {
-        Scanner scanner = new Scanner(System.in);
-        String inputJugador;
-        
-		do {
-			inputJugador = scanner.nextLine();
-		}while(!InputValido(inputJugador));
-		
-        return inputJugador;
-	}
-
-	public boolean InputValido(String input) {
-		if(NuevaCartaDelMazo(input)) {
-			return true;
-		}
-		if(CartaDelDescarteAMonton(input)) {
-			return true;
-		}
-		if(CartaDelMontonAlPilar(input)) {
-			return true;
-		}
-		if(MoverCartasEntreMontones(input)) {
-			return true;
-		}
-		System.out.println("Input no válido.\n");
-		return false;
-	}
-
-	private boolean NuevaCartaDelMazo(String input) {
-		return input.matches("c");
-	}
-	
-	private boolean CartaDelDescarteAMonton(String input) {
-		return input.matches("^d-[1-7]");
-	}
-	
-	private boolean CartaDelMontonAlPilar(String input) {
-		return input.matches("^[1-7]-p");
-	}
-
-	private boolean MoverCartasEntreMontones(String input) {
-		return input.matches("^[1-7]-[1-9][0-9]?-[1-7]");
+        return new Scanner(System.in).nextLine();
 	}
 
 	public void CantarVictoria() {
