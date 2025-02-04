eedc2bd230010b83f75c6791a09e66805c20c937
Juanjo Garrido Bascaran <juanjogarridobascaran@gmail.com>
2025-02-04 12:05:48 +0100

    refactor: Cambiar nombre variable

--- 2-ProgramacionOO/src/Pilares.java
+++ 2-ProgramacionOO/src/Pilares.java
@@ -2,36 +2,39 @@ import java.util.ArrayList;
 
 public class Pilares {
 	
-	private ArrayList<ArrayList<Carta>> PilaresCartas;
+	private ArrayList<ArrayList<Carta>> pilaresCartas;
 	
 	private int NUMERO_MONTONES = 4;
 	
 	public Pilares() {
-		PilaresCartas = new ArrayList<ArrayList<Carta>>();
+		pilaresCartas = new ArrayList<ArrayList<Carta>>();
 		
 		for(int i = 0; i < NUMERO_MONTONES; i++) {
-			PilaresCartas.add(new ArrayList<Carta>());
+			pilaresCartas.add(new ArrayList<Carta>());
 		}
 	}
 	
 	public boolean SePuedeAnadirCarta(Carta carta) {
-		Carta cartaPilar = PilaresCartas.get(carta.getPalo() - 1).getFirst();
+		if(pilaresCartas.get(carta.getPalo() - 1).isEmpty() && carta.getNumero() == 1) {
+			return true;
+		}
+		Carta cartaPilar = pilaresCartas.get(carta.getPalo() - 1).getFirst();
 		return carta.SonDelMismoColor(cartaPilar) && cartaPilar.EsInmediatamenteInferior(carta);
 	}
 
 	public void AnadirCarta(Carta carta) {
-		PilaresCartas.get(carta.getPalo() - 1).addFirst(carta);
+		pilaresCartas.get(carta.getPalo() - 1).addFirst(carta);
 	}
 	
 	public String Mostrar() {
 		String cartasEnPilares = "";
 		
 		for(int i = 0; i < NUMERO_MONTONES; i++) {
-			if(PilaresCartas.get(i).isEmpty()) {
+			if(pilaresCartas.get(i).isEmpty()) {
 				cartasEnPilares += "___";
 			}
 			else {
-				cartasEnPilares += PilaresCartas.get(i).getFirst().Mostrar();
+				cartasEnPilares += pilaresCartas.get(i).getFirst().Mostrar();
 			}
 			
 			cartasEnPilares += "  ";
