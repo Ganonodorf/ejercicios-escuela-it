be4f1617fb73fb0f2e0c0e7a7d515703725b18d3
Juanjo Garrido Bascaran <juanjogarridobascaran@gmail.com>
2025-02-04 09:06:10 +0100

    feat: Añadir métodos de Pilares

--- 2-ProgramacionOO/src/Pilares.java
+++ 2-ProgramacionOO/src/Pilares.java
@@ -1,4 +1,25 @@
+import java.util.ArrayList;
 
 public class Pilares {
+	
+	private ArrayList<ArrayList<Carta>> PilaresCartas;
+	
+	private int NUMERO_MONTONES = 4;
+	
+	public Pilares() {
+		PilaresCartas = new ArrayList<ArrayList<Carta>>();
+		
+		for(int i = 0; i < NUMERO_MONTONES; i++) {
+			PilaresCartas.add(new ArrayList<Carta>());
+		}
+	}
+	
+	public boolean SePuedeAnadirCarta(Carta carta) {
+		Carta cartaPilar = PilaresCartas.get(carta.getPalo() - 1).getFirst();
+		return carta.SonDelMismoColor(cartaPilar) && cartaPilar.EsInmediatamenteInferior(carta);
+	}
 
+	public void AnadirCarta(Carta carta) {
+		PilaresCartas.get(carta.getPalo() - 1).addFirst(carta);
+	}
 }
