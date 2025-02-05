c1b468945137d89aeb2a3b0c495ba85cafd5a83a
Juanjo Garrido Bascaran <juanjogarridobascaran@gmail.com>
2025-02-05 18:33:09 +0100

    refactor: Hacer que pilar herede de Mazo

---
+++ 2-ProgramacionOO/src/Pilar.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Pilar extends Mazo {
+	
+	@Override
+	public boolean SePuedeAnadirCarta(Carta carta) {
+		if(this.EstaVacio() && carta.EsRey()) {
+			return true;
+		}
+		
+		if(!this.EstaVacio() && this.VerPrimeracarta().EsInmediatamenteInferior(carta) && this.VerPrimeracarta().SonDelMismoPalo(carta)) {
+			return true;
+		}
+		
+		return false;
+	}
+}
