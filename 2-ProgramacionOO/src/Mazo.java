edbb75ed1e470d930d8b01848f8fb2be1fa9103a
Juanjo Garrido Bascaran <juanjogarridobascaran@gmail.com>
2025-02-05 18:13:44 +0100

    feat: Crear clase abstracta mazo para refactorizar

---
+++ 2-ProgramacionOO/src/Mazo.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public abstract class Mazo {
+	
+	private ArrayList<Carta> cartas;
+	
+	public Mazo() {
+	}
+	
+	public abstract boolean SePuedeAnadirCarta(Carta carta);
+	
+	public void AnadirCarta(Carta carta) {
+		cartas.addFirst(carta);
+	}
+	
+	public abstract void Mostrar();
+	
+	public boolean EstaVacio() {
+		return cartas.isEmpty();
+	}
+	
+	public Carta CogerPrimeraCarta() {
+		assert !this.EstaVacio();
+		Carta cartaADevolver = cartas.getFirst();
+		cartas.remove(0);
+		return cartaADevolver;
+	}
+}
