60bc9db3c10901a4ba1f361ed776a31dea8fc1b9
Juanjo Garrido Bascaran <juanjogarridobascaran@gmail.com>
2025-02-05 18:34:55 +0100

    refactor: Quitar método padre

--- 2-ProgramacionOO/src/Descarte.java
+++ 2-ProgramacionOO/src/Descarte.java
@@ -5,14 +5,4 @@ public class Descarte extends Mazo{
 	public boolean SePuedeAnadirCarta(Carta carta) {
 		return true;
 	}
-
-	@Override
-	public void Mostrar() {
-		if(!this.EstaVacio()) {
-			System.out.println(this.VerPrimeracarta().Mostrar());
-		}
-		else {
-			System.out.println("___");
-		}
-	}
 }
