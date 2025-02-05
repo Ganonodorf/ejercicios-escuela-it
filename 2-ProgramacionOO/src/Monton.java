57d9e8fdc3de7287b3d6987e29eaab1afc2a5904
Juanjo Garrido Bascaran <juanjogarridobascaran@gmail.com>
2025-02-05 19:31:40 +0100

    refactor: quitada responsabilidad a monton

--- 2-ProgramacionOO/src/Monton.java
+++ 2-ProgramacionOO/src/Monton.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-
 public class Monton extends Mazo{
 	
 	public Monton(int posicion, Baraja baraja) {
@@ -27,34 +25,5 @@ public class Monton extends Mazo{
 		}
 		
 		return false;
-	}
-
-	public boolean SePuedeMoverCarta(int montonOrigen, int posicionCarta, int montonDestino) {
-		Carta cartaSeleccionada = montonesCartas.get(montonOrigen - 1).get(posicionCarta - 1);
-		if(!cartaSeleccionada.EstaRevalada()) {
-			return false;
-		}
-		if(cartaSeleccionada.getNumero() == 13 && EstaElMontonVacio(montonDestino)) {
-			return true;
-		}
-		if(cartaSeleccionada.getNumero() != 13 && EstaElMontonVacio(montonDestino)) {
-			return false;
-		}
-		Carta cartaDestino = PrimeraCartaMonton(montonDestino);
-		if(cartaSeleccionada != null && cartaDestino != null &&
-				!cartaSeleccionada.SonDelMismoColor(cartaDestino) && cartaSeleccionada.EsInmediatamenteInferior(cartaDestino)){
-			return true;
-		}
-		return false;
-	}
-
-	public void MoverCartas(int montonOrigen, int posicionCarta, int montonDestino) {
-		for(int i = posicionCarta - 1; i >= 0; i--) {
-			montonesCartas.get(montonDestino - 1).addFirst(montonesCartas.get(montonOrigen - 1).get(i));
-			montonesCartas.get(montonOrigen - 1).remove(i);
-		}
-		if(!EstaElMontonVacio(montonOrigen)) {
-			PrimeraCartaMonton(montonOrigen).Revelar(true);
-		}
 	}
 }
