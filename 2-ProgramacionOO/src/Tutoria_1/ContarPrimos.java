c27bd97d2caf6594cd4386e0d63ee2235207e9de
Juanjo Garrido Bascaran <juanjogarridobascaran@gmail.com>
2025-02-11 17:19:33 +0100

    feat: Crear clase para contar primos de la tutoria 1

---
+++ 2-ProgramacionOO/src/Tutoria_1/ContarPrimos.java
@@ -0,0 +1,20 @@
+package Tutoria_1;
+
+public class ContarPrimos {
+	
+	public ContarPrimos() {
+		
+	}
+	
+	public int sumarPrimosEnLosPrimerosXNumeros(int tope) {
+		return 0;
+	}
+	
+	public int sumarLosPrimerosXPrimos(int numeroPrimos) {
+		return 0;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
