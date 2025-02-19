187881d9f80f81e9e16b2306cd1d65640c91cae5
Ganonodorf <juanjogarridobascaran@gmail.com>
2025-02-19 13:19:31 +0100

    feat: creation of method

--- 2-ProgramacionOO/src/Chess/Chess.java
+++ 2-ProgramacionOO/src/Chess/Chess.java
@@ -1,5 +1,20 @@
 package Chess;
 
 public class Chess {
-
+	
+	Board board = new Board();
+	
+	Player playerWhite = new Player();
+	
+	Player playerBlack = new Player();
+	
+	public Chess(){
+		
+	}
+	
+	public void play() {
+		do {
+			
+		}while(!board.isGameOver());
+	}
 }
