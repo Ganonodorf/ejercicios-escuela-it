c3b5f035f0b7856b53e6260fe40c8e2cb604d7cb
Juanjo Garrido Bascaran <juanjogarridobascaran@gmail.com>
2025-02-19 10:40:19 +0100

    feat: more functionality to coordinate class

--- 2-ProgramacionOO/src/Chess/Coordinate.java
+++ 2-ProgramacionOO/src/Chess/Coordinate.java
@@ -9,4 +9,32 @@ public class Coordinate {
 		this.x = x;
 		this.y = y;
 	}
+	
+	public boolean isInSameRow(Coordinate coordinate) {
+		return this.y == coordinate.y;
+	}
+	
+	public boolean isInSameColumn(Coordinate coordinate) {
+		return this.x == coordinate.x;
+	}
+	
+	public boolean isInPrimaryDiagonal(Coordinate coordinate) {
+		return this.x - coordinate.x == this.y - coordinate.y;
+	}
+	
+	public boolean isInSecondaryDiagonal(Coordinate coordinate) {
+		return this.x - coordinate.x == coordinate.y - this.y;
+	}
+	
+	public int distance(Coordinate coordinate) {
+		if(isInSameColumn(coordinate)) {
+			return Math.abs(this.y - coordinate.y);
+		}
+		
+		if(isInSameRow(coordinate) || isInPrimaryDiagonal(coordinate) || isInSecondaryDiagonal(coordinate)) {
+			return Math.abs(this.x - coordinate.x);
+		}
+		
+		return Math.abs(this.x - coordinate.x) + Math.abs(this.y - coordinate.y);
+	}
 }
