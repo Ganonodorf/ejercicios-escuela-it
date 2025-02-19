a31996c202421027a0e14e313cd4034ba406a4bf
Juanjo Garrido Bascaran <juanjogarridobascaran@gmail.com>
2025-02-19 10:13:09 +0100

    feat: Piece class created

--- 2-ProgramacionOO/src/Chess/Piece.java
+++ 2-ProgramacionOO/src/Chess/Piece.java
@@ -1,5 +1,16 @@
 package Chess;
 
-public class Piece {
-
+public abstract class Piece {
+	
+	private Coordinate coordinate;
+	
+	public Piece(Coordinate coordinate) {
+		this.coordinate = coordinate;
+	}
+	
+	public abstract boolean canMove(Coordinate coordinate);
+	
+	public abstract boolean canCapture(Coordinate coordinate);
+	
+	public abstract void move(Coordinate coordinate);
 }
