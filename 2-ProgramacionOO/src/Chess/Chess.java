595a70ba77dc7dd1ef260828f5a8a4c60ff35ec0
Juanjo Garrido Bascaran <juanjogarridobascaran@gmail.com>
2025-02-22 20:39:57 +0100

    feat: player and chess advance

--- 2-ProgramacionOO/src/Chess/Chess.java
+++ 2-ProgramacionOO/src/Chess/Chess.java
@@ -1,5 +1,7 @@
 package Chess;
 
+import java.util.Scanner;
+
 public class Chess {
 	
 	Board board = new Board();
@@ -14,7 +16,66 @@ public class Chess {
 	
 	public void play() {
 		do {
+			board = new Board();
 			
-		}while(!board.isGameOver());
+			do {
+				board.showInformation();
+				
+				board.proposeCombination(player.createCombination());
+			}while(board.numberOfAttemps() < NUMBER_OF_ATTEMPS && board.isWinnerResult() == false);
+
+			board.showInformation();
+			
+			if(board.isWinnerResult()) {
+				System.out.println("You've won!!! ;-)");
+			}
+			else {
+				System.out.println("You've lost!!! :-(");
+			}
+			
+		}while(playAgain() == true);
+	}
+
+	private boolean playAgain() {
+		String input;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		boolean goodAnswer;
+		
+		System.out.println("Do you want to continue? (y/n): ");
+
+		do {
+	        input = scanner.nextLine();
+	        
+	        goodAnswer = isGoodAnswer(input);
+	        
+	        if(!goodAnswer) {
+	        	System.out.println("Not a valid answer. Try again.");
+	        }
+	        
+		}while(!goodAnswer);
+		
+		return isPlayingAgain(input);
+	}
+	
+	private boolean isPlayingAgain(String input) {
+		if(input.matches("y")) {
+			return true;
+		}
+		return false;
+	}
+
+	private boolean isGoodAnswer(String input) {
+		if(input.matches(ANSWER_REGEX)) {
+			return true;
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+		MasterMind masterMind = new MasterMind();
+		
+		masterMind.play();
 	}
 }
--- 2-ProgramacionOO/src/Chess/Player.java
+++ 2-ProgramacionOO/src/Chess/Player.java
@@ -1,5 +1,47 @@
 package Chess;
 
-public class Player {
+import java.util.Scanner;
 
+public class Player {
+	
+	private String MOVE_REGEX = "^[kqrnbp][abcdefgh][1-8]x?[abcdefgh][1-8]$";
+	
+	public Player() {
+		
+	}
+	
+	public Move createMove() {
+		String input;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		boolean goodMove;
+
+		String error;
+
+		do {
+			System.out.println("move a piece, ex: qh4xe1");
+			
+	        input = scanner.nextLine();
+	        
+	        error = checkErrorInInput(input);
+	        
+	        if(error.length() > 0) {
+	        	System.out.println(error);
+	        }
+		}while(error.length() > 0);
+		
+		return inputToMove(input);
+	}
+
+	private String checkErrorInInput(String input) {
+		if(input.length() > 6) {
+			return "Wrong moved combination length";
+		}
+		if(!input.matches(MOVE_REGEX)) {
+			return "Wrong move, it must be like: qh4xe1";
+		}
+		
+		return "";
+	}
 }
