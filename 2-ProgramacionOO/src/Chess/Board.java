86553f5ccc4d3438d9f299f898ca86fbb7929b20
Juanjo Garrido Bascaran <juanjogarridobascaran@gmail.com>
2025-02-24 16:13:38 +0100

    fix: error in pieces creation on board

--- 2-ProgramacionOO/src/Chess/Board.java
+++ 2-ProgramacionOO/src/Chess/Board.java
@@ -49,7 +49,7 @@ public class Board {
 			initialNumber = origin.getY() < destiny.getY() ? origin.getY() : destiny.getY();
 			finalNumber = origin.getY() > destiny.getY() ? origin.getY() : destiny.getY();
 			
-			for(int i = initialNumber; i <= finalNumber; i++) {
+			for(int i = initialNumber + 1; i <= finalNumber; i++) {
 				if(spaces[origin.getX()][i] != null) {
 					return true;
 				}
@@ -59,7 +59,7 @@ public class Board {
 			initialNumber = origin.getX() < destiny.getX() ? origin.getX() : destiny.getX();
 			finalNumber = origin.getX() > destiny.getX() ? origin.getX() : destiny.getX();
 			
-			for(int i = initialNumber; i <= finalNumber; i++) {
+			for(int i = initialNumber + 1; i <= finalNumber; i++) {
 				if(spaces[origin.getY()][i] != null) {
 					return true;
 				}
@@ -119,33 +119,33 @@ public class Board {
 
 	private void createPieces() {
 		for(int i = 0; i < 8; i++) {
-			spaces[1][i] = new Pawn(new Coordinate(1, i), Color.WHITE);
-			spaces[6][i] = new Pawn(new Coordinate(6, i), Color.BLACK);
+			spaces[i][1] = new Pawn(new Coordinate(1, i), Color.WHITE);
+			spaces[i][6] = new Pawn(new Coordinate(6, i), Color.BLACK);
 		}
 		
 		spaces[0][0] = new Rook(new Coordinate(0, 0), Color.WHITE);
-		spaces[0][7] = new Rook(new Coordinate(0, 7), Color.WHITE);
+		spaces[7][0] = new Rook(new Coordinate(7, 0), Color.WHITE);
 		
-		spaces[0][1] = new Knight(new Coordinate(0, 1), Color.WHITE);
-		spaces[0][6] = new Knight(new Coordinate(0, 6), Color.WHITE);
+		spaces[1][0] = new Knight(new Coordinate(1, 0), Color.WHITE);
+		spaces[6][0] = new Knight(new Coordinate(6, 0), Color.WHITE);
 		
-		spaces[0][2] = new Bishop(new Coordinate(0, 2), Color.WHITE);
-		spaces[0][5] = new Bishop(new Coordinate(0, 5), Color.WHITE);
+		spaces[2][0] = new Bishop(new Coordinate(2, 0), Color.WHITE);
+		spaces[5][0] = new Bishop(new Coordinate(5, 0), Color.WHITE);
 		
-		spaces[0][3] = new Queen(new Coordinate(0, 3), Color.WHITE);
-		spaces[0][4] = new King(new Coordinate(0, 4), Color.WHITE);
+		spaces[3][0] = new Queen(new Coordinate(3, 0), Color.WHITE);
+		spaces[4][0] = new King(new Coordinate(4, 0), Color.WHITE);
 		
-		spaces[7][0] = new Rook(new Coordinate(0, 0), Color.BLACK);
-		spaces[7][7] = new Rook(new Coordinate(0, 7), Color.BLACK);
+		spaces[0][7] = new Rook(new Coordinate(0, 7), Color.BLACK);
+		spaces[7][7] = new Rook(new Coordinate(7, 7), Color.BLACK);
 		
-		spaces[7][1] = new Knight(new Coordinate(0, 1), Color.BLACK);
-		spaces[7][6] = new Knight(new Coordinate(0, 6), Color.BLACK);
+		spaces[1][7] = new Knight(new Coordinate(1, 7), Color.BLACK);
+		spaces[6][7] = new Knight(new Coordinate(6, 7), Color.BLACK);
 		
-		spaces[7][2] = new Bishop(new Coordinate(0, 2), Color.BLACK);
-		spaces[7][5] = new Bishop(new Coordinate(0, 5), Color.BLACK);
+		spaces[2][7] = new Bishop(new Coordinate(2, 7), Color.BLACK);
+		spaces[5][7] = new Bishop(new Coordinate(5, 7), Color.BLACK);
 		
-		spaces[7][3] = new Queen(new Coordinate(0, 3), Color.BLACK);
-		spaces[7][4] = new King(new Coordinate(0, 4), Color.BLACK);
+		spaces[3][7] = new Queen(new Coordinate(3, 7), Color.BLACK);
+		spaces[4][7] = new King(new Coordinate(4, 7), Color.BLACK);
 	}
 
 	public void showInformation() {
@@ -153,11 +153,11 @@ public class Board {
 			System.out.print(i + 1 + " ");
 			
 			for(int j = 0; j < 8; j++) {
-				if(spaces[i][j] == null) {
+				if(spaces[j][i] == null) {
 					System.out.print("_");
 				}
 				else {
-					spaces[i][j].Show();
+					spaces[j][i].Show();
 				}
 				System.out.print(" ");
 			}
@@ -186,8 +186,19 @@ public class Board {
 
 		Player player = new Player(Color.WHITE);
 		
-		Move move = player.createMove();
+		Move move = new Move("pc2c3", Color.WHITE);
 		
-		System.out.print(board.canMove(move));
+
+		System.out.println("Spaces: " + board.spaces[2][1]);
+		System.out.println("Is piece: " + board.isPiece(move.piece.coordinate));
+		System.out.println("Piece in: " + board.pieceIn(move.piece.coordinate));
+		System.out.println("move.piece.coordinate: " + move.piece.coordinate.getX() + " " + move.piece.coordinate.getY());
+		System.out.println("move.destiny: " + move.destiny.getX() + " " + move.destiny.getY());
+		System.out.println("Piece in can move: " + board.pieceIn(move.piece.coordinate).canMove(move.destiny));
+		System.out.println("Is a p on the way: " + board.isAPieceOnTheWay(move.piece.coordinate, move.destiny));
+		System.out.println("Can move p: " + board.canMovePiece(move.piece.coordinate, move.destiny));
+		System.out.println("Can move: " + board.canMove(move));
+
+		//System.out.println(board.canMovePiece(move.piece.coordinate, move.destiny));
 	}
 }
